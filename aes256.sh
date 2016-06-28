e5e5a15684214d01f382db6437b925b1e5565267
Zuzzuc <loveborgstrom98@gmail.com>
2016-06-28 20:07:13 +0200

    Update aes256.sh

--- aes256.sh
+++ aes256.sh
@@ -5,41 +5,27 @@ TRG=$1 && SRC=$1
 if [ "$1" != "" ];then
 	if [ "$2" == "enc" ];then
 		if [[ -d $1 ]];then
-			echo in dir
-			
 			mv $1 ./
-			zip -r "$(basename $1).zip" "$(basename $1)" 
+			zip -rq "$(basename $1).zip" "$(basename $1)" 
 			SRC="$ws/$(basename $1).zip"
-			echo SRC IS: $SRC
-			echo basename is : $(basename $1)
 		fi
-		echo in enc
 		openssl aes-256-cbc -a -salt -in $SRC -out $TRG -pass file:<( echo -n "$3" )
 	elif [ "$2" == "dec" ];then
-	echo in dec
 		mv $1 ./
 		SRC="$ws/$(basename $1)"
 		openssl aes-256-cbc -d -a -salt -in $SRC -out $TRG -pass file:<( echo -n "$3" )
-		sleep 1
-		
-		
 		if grep -q "PK" <<<"$(awk 'FNR == 1 { print; exit }' $TRG)"; then
 			rm "$ws/$(basename $TRG)"
 			mv "$TRG" "$ws/$(basename $TRG)"
 			TRG="$ws/$TRG"
-			echo "It's there!!!!!!!!!!!!!!!!!!!!!!!!!!!1?!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-			echo im in unzip
-			echo ITTTTT WOOOOORLSSSS
-			sleep 2
 			mkdir "$1"
-			unzip -o "$(basename $TRG)" -d "$(cd $1 && cd .. && pwd)"
+			unzip -o -qq "$(basename $TRG)" -d "$(cd $1 && cd .. && pwd)"
 		else
 			echo "FAIL, its ..$(awk 'FNR == 1 { print; exit }' $TRG).."
     	fi
-		echo post unzip
 		sleep 5
 	else
-		echo "incorrect operation"
+		echo "Incorrect operation"
 	fi
 else
 	echo "No path specificated"
