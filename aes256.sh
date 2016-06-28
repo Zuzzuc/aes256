4dcdf13e229949db86e51c2336a15f02b9475afd
Zuzzuc <loveborgstrom98@gmail.com>
2016-06-28 20:06:08 +0200

    Update aes256.sh

--- aes256.sh
+++ aes256.sh
@@ -1,11 +1,47 @@
 #!/bin/bash
-# Author Zuzzuc github.com/Zuzzuc/
+# Author Zuzzuc https://github.com/Zuzzuc/
+ws=/tmp/$RANDOM && mkdir $ws && cd $ws
+TRG=$1 && SRC=$1
 if [ "$1" != "" ];then
-	if [ "$3" == "enc" ];then
-		openssl aes-256-cbc -a -salt -in $1 -out $2 -pass file:<( echo -n "$4" )
-	elif [ "$3" == "dec" ];then
-		openssl aes-256-cbc -d -a -salt -in $1 -out $2 -pass file:<( echo -n "$4" )
+	if [ "$2" == "enc" ];then
+		if [[ -d $1 ]];then
+			echo in dir
+			
+			mv $1 ./
+			zip -r "$(basename $1).zip" "$(basename $1)" 
+			SRC="$ws/$(basename $1).zip"
+			echo SRC IS: $SRC
+			echo basename is : $(basename $1)
+		fi
+		echo in enc
+		openssl aes-256-cbc -a -salt -in $SRC -out $TRG -pass file:<( echo -n "$3" )
+	elif [ "$2" == "dec" ];then
+	echo in dec
+		mv $1 ./
+		SRC="$ws/$(basename $1)"
+		openssl aes-256-cbc -d -a -salt -in $SRC -out $TRG -pass file:<( echo -n "$3" )
+		sleep 1
+		
+		
+		if grep -q "PK" <<<"$(awk 'FNR == 1 { print; exit }' $TRG)"; then
+			rm "$ws/$(basename $TRG)"
+			mv "$TRG" "$ws/$(basename $TRG)"
+			TRG="$ws/$TRG"
+			echo "It's there!!!!!!!!!!!!!!!!!!!!!!!!!!!1?!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+			echo im in unzip
+			echo ITTTTT WOOOOORLSSSS
+			sleep 2
+			mkdir "$1"
+			unzip -o "$(basename $TRG)" -d "$(cd $1 && cd .. && pwd)"
+		else
+			echo "FAIL, its ..$(awk 'FNR == 1 { print; exit }' $TRG).."
+    	fi
+		echo post unzip
+		sleep 5
+	else
+		echo "incorrect operation"
 	fi
 else
-	echo "Incorrect argument(s)"
+	echo "No path specificated"
 fi
+rm -rf $ws
