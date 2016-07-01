4fd881efedb1901edc5dd132a9243ec5d297a68e
Zuzzuc <loveborgstrom98@gmail.com>
2016-07-01 17:44:15 +0200

    Update AES256.sh

--- AES256.sh
+++ AES256.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
 # Author Zuzzuc https://github.com/Zuzzuc/
-# Does not work when path contains spaces. Will fix soon
 ws=/tmp/$RANDOM && mkdir $ws && cd $ws
-TRG=$1 && SRC=$1
-if [ "$1" != "" ];then
-	if [ "$2" == "enc" ];then
-		if [[ -d $1 ]];then
-			mv $1 ./
-			zip -rq "$(basename $1).zip" "$(basename $1)" 
-			SRC="$ws/$(basename $1).zip"
-		fi
-		openssl aes-256-cbc -a -salt -in $SRC -out $TRG -pass file:<( echo -n "$3" )
-	elif [ "$2" == "dec" ];then
-		mv $1 ./
-		SRC="$ws/$(basename $1)"
-		openssl aes-256-cbc -d -a -salt -in $SRC -out $TRG -pass file:<( echo -n "$3" )
-		if grep -q "PK" <<<"$(awk 'FNR == 1 { print; exit }' $TRG)"; then
-			rm "$ws/$(basename $TRG)"
-			mv "$TRG" "$ws/$(basename $TRG)"
-			TRG="$ws/$TRG"
-			mkdir "$1"
-			unzip -o -qq "$(basename $TRG)" -d "$(cd $1 && cd .. && pwd)"
-    	fi
-		sleep 5
-	else
-		echo "Incorrect operation"
-	fi
+SRC=$1 && TRUESRC=$(echo "$1" | sed 's%\\%%g') && TRUESRC=$(echo "$TRUESRC" | sed -e 's%[[:space:]]*$%%') && CUR_PATH="$TRUESRC"
+F_IS_DIR=-1;
+if [ "$1" == "" ] || [ "$2" == "" ] || [ "$3" == "" ];then
+	echo "Incorrect usage"
+	exit 1
 else
-	echo "No path specificated"
+	F_NAME="${TRUESRC##*/}"
+	if [ "$2" == "enc" ];then
+		if [ -d "$TRUESRC" ];then
+			F_IS_DIR=1;
+		else
+			F_IS_DIR=0;
+		fi
+		mv "$TRUESRC" ./ 
+		if [[ F_IS_DIR -eq 1 ]];then
+			zip -rq "$F_NAME".zip "$F_NAME"
+			openssl aes-256-cbc -a -salt -in "$ws/$F_NAME.zip" -out "$ws/enc" -pass file:<( echo -n "$3" )
+			mv "$ws/enc" "$TRUESRC"
+		else
+			openssl aes-256-cbc -a -salt -in "$ws/$F_NAME" -out "$ws/enc" -pass file:<( echo -n "$3" )
+			mv "$ws/enc" "$TRUESRC"
+		fi
+	elif [ "$2" == "dec" ];then
+		openssl aes-256-cbc -d -a -salt -in "$TRUESRC" -out "$ws/DEC" -pass file:<( echo -n "$3" )
+		rm -rf "$TRUESRC"
+		if [[ $(head -1 "DEC" | grep -q "PK" && echo "isDir") == "isDir" ]];then
+			unzip -o -qq "DEC" -d "DEC.d"
+			mv "DEC.d/${TRUESRC##*/}" "$TRUESRC"
+		else
+			rm -rf "$TRUESRC"
+			mv "DEC" "$TRUESRC"
+		fi
+	fi
 fi
 rm -rf $ws
+exit 0
